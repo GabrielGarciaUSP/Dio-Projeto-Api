02dc78bd61125d76cd7791861d26504ff78cf856
Gabriel Garcia <gabrielgarcia.ata@gmail.com>
2024-07-26 16:57:00 -0300

    First Commig

---
+++ src/main/java/com/meuprojeto/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.meuprojeto.api.controller;
+
+
+
+import com.meuprojeto.api.model.User;
+import com.meuprojeto.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
+
